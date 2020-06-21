4f864bd258b041ab2e942384f760e8cf124c2475
munishgarg02 <munishkr.garg@yahoo.in>
2020-06-21 15:06:33 +1300

    path changed

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('home',views.home,name='home'),
     path('registration',views.registration,name='registration'),
     path('patient',views.patient,name='patient'),
     path('login',views.LogIn,name='login'),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('logout',views.LogOut,name='logout'),
     path('about',views.about,name='about'),
     path('sms',views.sms,name='sms')
+    path('',views.home,name='home'),
 ]
